cace29874ef7fd6c05fa8701525b083ef1141f6f
ekisu <dts.ramon@gmail.com>
2020-03-07 17:57:33 -0300

    Revert "BootLiquor: Simplify output protocols"
    This reverts commit a2e07526cae24f1bb93b4daa0d322f10e8fe79e6.

--- Platform/BootLiquor/Output/OutputStGop.c
+++ Platform/BootLiquor/Output/OutputStGop.c
@@ -10,63 +10,20 @@
 #include <Protocol/GraphicsOutput.h>
 
 #include <Library/DebugLib.h>
-#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
 #include "../GuiIo.h"
 
 struct GUI_OUTPUT_CONTEXT_ {
-  EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
+  EFI_GRAPHICS_OUTPUT_PROTOCOL Gop;
 };
-
-STATIC
-EFI_GRAPHICS_OUTPUT_PROTOCOL *
-InternalGuiOutputLocateGop (
-  VOID
-  )
-{
-  EFI_STATUS                   Status;
-  EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
-
-  Status = gBS->HandleProtocol (
-    gST->ConsoleOutHandle,
-    &gEfiGraphicsOutputProtocolGuid,
-    (VOID **) &Gop
-    );
-  if (EFI_ERROR (Status)) {
-    Status = gBS->LocateProtocol (
-      &gEfiGraphicsOutputProtocolGuid,
-      NULL,
-      (VOID **) &Gop
-      );
-    if (EFI_ERROR (Status)) {
-      return NULL;
-    }
-  }
-
-  return Gop;
-}
 
 GUI_OUTPUT_CONTEXT *
 GuiOutputConstruct (
   VOID
   )
 {
-  GUI_OUTPUT_CONTEXT            *Context;
-  EFI_GRAPHICS_OUTPUT_PROTOCOL  *Gop;
-
-  Gop = InternalGuiOutputLocateGop();
-  if (Gop == NULL) {
-    return NULL;
-  }
-
-  Context = AllocatePool (sizeof (*Context));
-  if (Context == NULL) {
-    return NULL;
-  }
-
-  Context->Gop = Gop;
-  return Context;
+  return GuiOutputConstructStGop ();
 }
 
 EFI_STATUS
@@ -84,18 +41,18 @@ GuiOutputBlt (
   IN UINTN                              Delta OPTIONAL
   )
 {
-  return Context->Gop->Blt (
-    Context->Gop,
-    BltBuffer,
-    BltOperation,
-    SourceX,
-    SourceY,
-    DestinationX,
-    DestinationY,
-    Width,
-    Height,
-    Delta
-    );
+  return GuiOutputBltStGop (
+           Context,
+           BltBuffer,
+           BltOperation,
+           SourceX,
+           SourceY,
+           DestinationX,
+           DestinationY,
+           Width,
+           Height,
+           Delta
+           );
 }
 
 CONST EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *
@@ -103,7 +60,7 @@ GuiOutputGetInfo (
   IN GUI_OUTPUT_CONTEXT  *Context
   )
 {
-  return Context->Gop->Mode->Info;
+  return GuiOutputGetInfo (Context);
 }
 
 VOID
@@ -112,5 +69,5 @@ GuiOutputDestruct (
   )
 {
   ASSERT (Context != NULL);
-  FreePool (Context);
+  GuiOutputDestructStGop (Context);
 }
